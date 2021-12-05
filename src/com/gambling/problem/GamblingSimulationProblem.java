e05072470d9af8b289179dcfd60de084f2728890
deepakphpweb <dipakrana2760@gmail.com>
2021-12-05 17:07:22 +0530

    Added : Attached .gitignore file and create a gambling simulation problem class

---
+++ src/com/gambling/problem/GamblingSimulationProblem.java
@@ -0,0 +1,12 @@
+package com.gambling.problem;
+
+public class GamblingSimulationProblem {
+    public static void main(String[] args) {
+
+        System.out.println("Welcome To Gambling Simulator");
+
+        //constants
+        int BET=1;
+        int STAKE=100;
+    }
+}
